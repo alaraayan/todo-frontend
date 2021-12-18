57dd8e28d269fb1143ee05a0b02ef3d4529e5cf7
alara <alaraayan@hotmail.com>
2021-12-18 15:42:13 +0000

    profile image done

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -7,8 +7,17 @@ import { registerUser, loginUser } from '../../lib/api'
 import { setToken } from '../../lib/auth'
 
 function Register() {
+  const quoteData = [
+    'I am a freelancer, which is pretty much a modern day cowboy.',
+    'Rejection from society is what created the X-Men.',
+    'Realizations are the worst.',
+    'Lovers... Oh, that word bums me out unless it is between meat and pizza.',
+    'All of humankind has one thing in common: the sandwich. I believe that all anyone really wants in this life is to sit in peace and eat a sandwich.'
+  ]
+
   const navigate = useNavigate()
   const [error, setError] = React.useState('')
+  const [hasImage, setHasImage] = React.useState(false)
   const { formData, formErrors, handleChange } = useForm({
     username: '',
     email: '',
@@ -35,91 +44,112 @@ function Register() {
     }
   }
 
+  const handleImage = () => {
+    setHasImage(true)
+  }
+
+  // console.log(hasImage)
+  // console.log(formData)
+
   return (
     <section className="user-forms">
-      <section className="form-container">
-        <h1 className="user-form">Create a new account</h1>
-        <form className="user-form" onSubmit={handleSubmit}>
-          <div>
-            {/* <label>Username</label> */}
-            <input
-              className="user-form user-info"
-              placeholder="Username"
-              onChange={handleChange}
-              name="username"
-              value={formData.username}
-            />
-            {formErrors.username && (
-              <p>{formErrors.username}</p>
-            )}
-          </div>
-          <div>
-            {/* <label>Email</label> */}
-            <input
-              className="user-form user-info"
-              placeholder="Email"
-              onChange={handleChange}
-              name="email"
-              value={formData.email}
-            />
-            {formErrors.email && (
-              <p>{formErrors.email}</p>
-            )}
-          </div>
-          <div>
-            {/* <label>Password</label> */}
-            <input
-              className="user-form user-info"
-              type="password"
-              placeholder="Password"
-              onChange={handleChange}
-              name="password"
-              value={formData.password}
-            />
-            {formErrors.password && (
-              <p>{formErrors.password}</p>
-            )}
-          </div>
-          <div>
-            {/* <label>Password Confirmation</label> */}
-            <input
-              className="user-form user-info"
-              type="password"
-              placeholder="Password Confirmation"
-              onChange={handleChange}
-              name="passwordConfirmation"
-              value={formData.passwordConfirmation}
-            />
-            {formErrors.passwordConfirmation && (
-              <p className="user-form">{formErrors.passwordConfirmation}</p>
-            )}
-          </div>
-          <div>
-            {/* <label>Profile Image</label> */}
-            <input
-              className="user-form user-info"
-              placeholder="Profile Image"
-              onChange={handleChange}
-              name="profileImage"
-              value={formData.profileImage}
-            />
-            {formErrors.profileImage && (
-              <p className="user-form">{formErrors.profileImage}</p>
-            )}
-          </div>
-          {error && <p>{error}</p>}
-          <div>
-            <button type="submit" className="user-form submit-button">
+      <form className="user-form" onSubmit={handleSubmit}>
+        <div className={hasImage ? 'no-show' : ''}>
+          <section className="form-container">
+            <h1 className="user-form">Create a new account</h1>
+          
+            <div>
+              {/* <label>Username</label> */}
+              <input
+                className="user-form user-info"
+                placeholder="Username"
+                onChange={handleChange}
+                name="username"
+                value={formData.username}
+              />
+              {formErrors.username && (
+                <p>{formErrors.username}</p>
+              )}
+            </div>
+            <div>
+              {/* <label>Email</label> */}
+              <input
+                className="user-form user-info"
+                placeholder="Email"
+                onChange={handleChange}
+                name="email"
+                value={formData.email}
+              />
+              {formErrors.email && (
+                <p>{formErrors.email}</p>
+              )}
+            </div>
+            <div>
+              {/* <label>Password</label> */}
+              <input
+                className="user-form user-info"
+                type="password"
+                placeholder="Password"
+                onChange={handleChange}
+                name="password"
+                value={formData.password}
+              />
+              {formErrors.password && (
+                <p>{formErrors.password}</p>
+              )}
+            </div>
+            <div>
+              {/* <label>Password Confirmation</label> */}
+              <input
+                className="user-form user-info"
+                type="password"
+                placeholder="Password Confirmation"
+                onChange={handleChange}
+                name="passwordConfirmation"
+                value={formData.passwordConfirmation}
+              />
+              {formErrors.passwordConfirmation && (
+                <p className="user-form">{formErrors.passwordConfirmation}</p>
+              )}
+            </div>
+            {error && <p>{error}</p>}
+            <div>
+              <button type="click" className="user-form submit-button" onClick={handleImage}>
             Register
-            </button>
+              </button>
             
-          </div>
-        </form>
-        <footer>
-          <h5>Already a member? <span><Link to="/login">Login instead.</Link></span> </h5>
-        </footer>
-      </section>
-      <ToastContainer />
+            </div>
+          
+            <footer>
+              <h5>Already a member? <span><Link to="/login">Login instead.</Link></span> </h5>
+            </footer>
+        
+          </section>
+          <ToastContainer />
+        </div>
+        <div className={!hasImage ? 'no-show' : ''}>
+          <section className="form-container">
+            <h1 className="user-form">Choose a Liz Lemon quote:</h1>
+            {quoteData.map((quote, index) => {
+              return <button
+                key={index}
+                type="button"
+                className="user-info quote-buttons" 
+                name="profileImage" 
+                value={index}
+                onClick={handleChange}> 
+                {quote}
+              </button>
+            })}
+            <div>
+              <button type="submit" className="user-form submit-button">
+              Choose
+              </button>
+            
+            </div>
+          </section>
+        </div>
+      </form>
     </section>
   )
 }
